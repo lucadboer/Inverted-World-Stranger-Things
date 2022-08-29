4e698a0a744d2ed7f4d9d8cf5f71d47a8184c362
Luca Destefano B <lucahesper@hotmail.com>
2022-08-29 09:02:09 -0300

    up files

---
+++ Inverted-World-Stranger-Things/assets/js/main.js
@@ -0,0 +1,56 @@
+import { subscribeToHellfireClub } from './FireBase/hellfire-club.js';
+console.log('foiii');
+
+const btnInventerMundos = document.querySelector('#switch-theme-button')
+const formDeD = document.querySelector('#form-DeD')
+const music = document.querySelector('#music')
+
+const txtName = document.querySelector('#txtName')
+const txtEmail = document.querySelector('#txtEmail')
+const txtLevel = document.querySelector('#txtLevel')
+const txtCharacter = document.querySelector('#txtCharacter')
+const btnSubscribe = document.querySelector('#btnSubscribe')
+
+window.addEventListener('load', playMusic)
+
+function playMusic() {
+    const theme = document.body.classList[0]
+    const musicTheme = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
+
+    music.src = `assets/musics/${musicTheme}`
+    music.play()
+    audio.volume = 0.2
+}
+
+btnInventerMundos.addEventListener('click', () => {
+    console.log('eai');
+    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('light-theme')
+
+    playMusic()
+
+})
+
+btnSubscribe.addEventListener('click', enviarFormulario)
+
+function enviarFormulario() {
+    const subscribes = {
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        character: txtCharacter.value
+    }
+
+    formDeD.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        subscribeToHellfireClub(subscribes) 
+        swal("Player cadastrado!", "", "success")
+        .then(() => {
+            txtName.value = ''
+            txtEmail.value = ''
+            txtLevel.value = ''
+            txtCharacter.value = ''
+        })
+    })
+}
+
